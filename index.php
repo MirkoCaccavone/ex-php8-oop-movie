70b62f9be951c1cecb48e97f2d705a2dec66be8e
Mirko <mirkocaccavone03@gmail.com>
2025-05-05 18:53:55 +0200

    [feat] add movie display layout and styles

--- index.php
+++ index.php
@@ -2,50 +2,9 @@
 
 require_once './traits/MovieDetails.php';
 
-// Definisco la classe Genre
-class Genre {
-    // Definisco la classe Genre con le proprietà name e description
-    public $name;
-    public $description;
+require_once './models/Genre.php';
 
-    // Costruttore per inizializzare le proprietà
-    public function __construct($_name, $_description) {
-        $this->name = $_name;
-        $this->description = $_description;
-    }
-}
-
-// Definisco la classe Movie
-class Movie {
-    use MovieDetails;
-
-    // Definisco la classe Movie con le proprietà title, year, genre e director
-    public $title;
-    public $year;
-    public $genres;
-    public $director;
-
-    // Costruttore per inizializzare le proprietà
-    public function __construct($_title, $_year, $_genres, $_director) {
-        $this->title = $_title;
-        $this->year = $_year;
-        $this->genres = is_array($_genres) ? $_genres : [$_genres];
-        $this->director = $_director;
-    }
-
-    // Metodo per ottenere le informazioni del film
-    public function getInfo() {
-        // Creo la stringa dei generi
-        $genreStrings = array_map(function($genre) {
-            return "{$genre->name} ({$genre->description})";
-        }, $this->genres);
-        
-        $genresString = implode(', ', $genreStrings);
-        
-        return "Titolo: {$this->title}, Anno: {$this->year}, Generi: {$genresString}, " . 
-               "Regista: {$this->director}, " . $this->getMovieDetails();
-    }
-}
+require_once './models/Movie.php';
 
 // Creo dei generi
 $action = new Genre("Azione", "Film con scene d'azione e adrenalina");
@@ -60,22 +19,44 @@ $movie1->setMovieDetails(8.2, 28);
 $movie2 = new Movie("La vita è bella", 1997, $comedy, "Roberto Benigni");
 $movie2->setMovieDetails(8.6, 20);
 
-// Stampo le informazioni dei film
-echo $movie1->getInfo() . "<br>";
-echo $movie2->getInfo();
+// // Stampo le informazioni dei film
+// echo $movie1->getInfo() . "<br>";
+// echo $movie2->getInfo();
+
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OOP movie</title>
+    <title>Movie List</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     
-    
+    <h1>I Nostri Film</h1>
 
-    
+    <div class="movie-grid">
+        <?php foreach ([$movie1, $movie2] as $movie): ?>
+            <div class="movie-card">
+                <h2 class="movie-title"><?php echo $movie->title; ?></h2>
+                <div class="movie-info">
+                    <p>Anno: <?php echo $movie->year; ?></p>
+                    <p>Regista: <?php echo $movie->director; ?></p>
+                </div>
+                <div class="movie-genres">
+                    <?php foreach ($movie->genres as $genre): ?>
+                        <span class="genre-tag"><?php echo $genre->name; ?></span>
+                    <?php endforeach; ?>
+                </div>
+                <div class="movie-details">
+                    <?php echo $movie->getMovieDetails(); ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </body>
 </html>
